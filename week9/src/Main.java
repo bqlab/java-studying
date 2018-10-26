f4d78a6eb5e68347928e108ac5ab091427c9b366
BQ Lab <beaqon@naver.com>
2018-10-26 11:46:50 +0900

    수업끝 코딩한거 없음

--- week9/src/Main.java
+++ week9/src/Main.java
@@ -1,10 +1,13 @@
 import java.util.*;
-import animal.Animal;;
+import animal.*;
+import pet.Cat;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in)
+		Scanner in = new Scanner(System.in);
 		Animal a = new Animal();
+		Dog d = new Dog();
+		Cat c = new Cat();
 	}
 }
