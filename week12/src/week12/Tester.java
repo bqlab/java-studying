f5dd84bc325703c6984980416689349a6a655c3e
BQ Lab <beaqon@naver.com>
2018-11-09 09:27:03 +0900

    존나지친다

---
+++ week12/src/week12/Tester.java
@@ -0,0 +1,18 @@
+package week12;
+
+import javax.swing.JFrame;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Tester {
+
+	public void main() {
+		JFrame jframe = new JFrame();
+		JTextField button = new JTextField("press me");
+		
+		jframe.setSize(500, 300);
+		jframe.setTitle("Good Morning");
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setVisible(true);
+	}
+}
