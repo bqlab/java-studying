f8f664fd77eeca3f26220c922c420ac3abe08782
BQ Lab <beaqon@naver.com>
2018-11-09 12:16:36 +0900

    dd

--- week12/src/MyFriendsList.java
+++ week12/src/MyFriendsList.java
@@ -1,10 +1,21 @@
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 
-
-public class MyFriendsList {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class MyFriendsList extends JFrame {
+	public MyFriendsList() {
+		setSize(300, 400);
+		setTitle("list");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		makeUI();
+		setVisible(true);
 	}
-
+	private void makeUI() {
+		String[] friends = {"hone", "lee", "kim" };
+		JList<String> list = new JList<String>(friends);
+		add(new JScrollPane(list));
+	}
+	public static void main(String[] args) {
+		MyFriendsList list = new MyFriendsList();
+	}
 }
