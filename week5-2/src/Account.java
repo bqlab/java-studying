e88159878c430932c9d8e840e1edca9ef75d44bf
BQ Lab <beaqon@naver.com>
2018-11-22 02:48:57 +0900

    -

--- week5-2/src/Account.java
+++ week5-2/src/Account.java
@@ -1,4 +1,12 @@
-
 public class Account {
-
+	int balance;
+	public void deposit(int a) {
+		balance = balance + a;
+	}
+	public void withraw(int a) {
+		balance = balance - a;
+	}
+	public int getBalance() {
+		return balance;
+	}
 }
