519c1b6848336e9de75b3f5413f27ffc868d0eed
BQ Lab <beaqon@naver.com>
2018-11-22 02:45:00 +0900

    Configured bubble sort in 5-2

--- week5-2/src/Tester.java
+++ week5-2/src/Tester.java
@@ -1,9 +1,38 @@
+import java.util.Random;
 
 public class Tester {
+	static int[] numbers = new int[10];
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("정렬전");
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = new Random().nextInt(99);
+			System.out.println(numbers[i]);
+		}
 
+		bubbleSort();
+		System.out.println("정렬후");
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
 	}
 
+	public static void bubbleSort() {
+		for (int i = 0; i < numbers.length - 1; i++) {
+			for (int j = 0; j < numbers.length - 1 - i; j++)
+				if (isGreater(j, j + 1))
+					swap(j, j + 1);
+		}
+	}
+
+	public static void swap(int a, int b) {
+		int temp;
+		temp = numbers[a];
+		numbers[a] = numbers[b];
+		numbers[b] = temp;
+	}
+
+	public static boolean isGreater(int a, int b) {
+		return numbers[a] > numbers[b];
+	}
 }
