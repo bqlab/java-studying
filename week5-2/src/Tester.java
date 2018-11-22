215912e14a0742a5618f3a87f4bc7a051764e1b8
BQ Lab <beaqon@naver.com>
2018-11-22 16:11:04 +0900

    Solved until 6 number in 5-2

--- week5-2/src/Tester.java
+++ week5-2/src/Tester.java
@@ -1,38 +1,26 @@
 import java.util.Random;
 
 public class Tester {
-	static Account[] accounts = new Account[10];
+	static Account[] accounts;
+	static Sorter sorter;
 
 	public static void main(String[] args) {
+		int[] balances = new int[10];
+		accounts = new Account[10];
 		System.out.println("정렬전");
 		for (int i = 0; i < accounts.length; i++) {
 			accounts[i] = new Account();
 			accounts[i].setBalance(new Random().nextInt(99));
-			System.out.println(accounts[i].getBalance());
+			balances[i] = accounts[i].getBalance();
+			System.out.println(balances[i]);
 		}
-tlqkf
-		bubbleSort();
+
+		sorter = new Sorter(balances);
+		balances = sorter.start();
 		System.out.println("정렬후");
 		for (int i = 0; i < accounts.length; i++) {
+			accounts[i].setBalance(balances[i]);
 			System.out.println(accounts[i].getBalance());
 		}
-	}
-
-	public static void bubbleSort() {
-		for (int i = 0; i < accounts.length - 1; i++) {
-			for (int j = 0; j < accounts.length - 1 - i; j++)
-				if (isGreater(j, j + 1))
-					swap(j, j + 1);
-		}
-	}
-
-	public static void swap(int a, int b) {
-		int temp = accounts[a].getBalance();
-		accounts[a].setBalance(accounts[b].balance);
-		accounts[b].setBalance(temp);
-	}
-
-	public static boolean isGreater(int a, int b) {
-		return accounts[a].getBalance() > accounts[b].getBalance();
 	}
 }
---
+++ week5-2/src/bubbleSort.java
@@ -0,0 +1,7 @@
+
+public interface bubbleSort {
+	public int[] numbers = new int[10];
+	public int[] start();
+	public void swap(int a, int b);
+	public boolean isGreater(int a, int b);
+}
